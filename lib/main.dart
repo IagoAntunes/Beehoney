a91edb1afa3c0c68cc0f92a03ad603c16e6e1bdd
IagoAntunes <iagoantunes.f@gmail.com>
2023-01-18 11:01:07 -0300

    infinite background

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ void main() {
 class BeeHoney extends FlameGame {
   // SpriteComponent -> Definir Posição Cor tipo imagem tamanho
   Bg bg = Bg();
+  Bg bg2 = Bg();
   SpriteComponent bee = SpriteComponent();
   @override
   Future<void>? onLoad() async {
@@ -19,6 +20,12 @@ class BeeHoney extends FlameGame {
       ..position = Vector2(0, 0); // Posicao em X | Y
 
     add(bg); // Adicione o objeto na tela
+    add(bg2);
+    bg2
+      ..sprite = await Sprite.load("bg.png")
+      ..size.x = 500 // tamanho em X
+      ..size.y = 900 //tamanho em Y
+      ..position = Vector2(0, -900); //
 
     bee
       ..sprite = await Sprite.load("bee1.png")
@@ -33,13 +40,17 @@ class BeeHoney extends FlameGame {
   @override
   void update(double dt) {
     //delta time- independente do nivel de processamento
-    bg.move(dt);
+    bg.move(dt, 100, 900, 0.0);
+    bg2.move(dt, 100, 0, -900.0);
     super.update(dt);
   }
 }
 
 class Bg extends SpriteComponent {
-  move(dt) {
-    y += 100 * dt; //Pega posicao eixo y adiciona 1
+  move(dt, speed, limit, posy) {
+    y += speed * dt; //Pega posicao eixo y adiciona 1
+    if (y >= limit) {
+      y = posy;
+    }
   }
 }
